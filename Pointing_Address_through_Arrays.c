9b403c725d2faddf8de90cf3e1dbb497a6d1457d
Raushan <raushanmandallkr@gmail.com>
2023-08-17 02:35:06 -0700

    Taking inputs from user in an array[with Pointers]

--- Pointing_Address_through_Arrays.c
+++ Pointing_Address_through_Arrays.c
@@ -19,23 +19,28 @@ int main()
     for (int i = 0; i < 4; i++)
     {
         printf("Enter the value of marks for student %d\n", i + 1); // Stdent 0 se print n ho isilye bas Hum [i+1] kar rhen
-        scanf("%d", marks); // yahan hum apne inputs user se lekar apne Array me store directly nhi karwayenge, woh kyun dekhte hain -> 
-                            // scanf("%d", st_marks); Agar hum apne pointer VAriable me store n karakar agar apne [ARRAY] store ->
-                            // karwate hain to humari value, us array ke andar store to ho jayengi, LEKIN SIRF AUR SIRF AAKHRI ->
-                            // VALUE HI STORE HOGI, HUM JITNI BAAR APNI VALLUE DENGE WAH BAS IK HI ADDRESS PE STORE HONGI AUR ->
-                            // HUMESHA WAHIN PE VALUES CHANGE HOTE RAHGENG [ KYUNKI, ARRAY ik tarah ka Pointer hi hai] AUR ->
-                            // HUM JITNI BAAR VALUES ENTER KARTE JAYENGE WAH CHANGE HOTE JAYENGE, AUR SABSE AAKHRI VALUE ->
-                            // US IK HI ADDRESS PE STORE HOGI. For Example - st_marks[0] = 455;
-                            // Hum sirf ik hi array me values ko change karte jayenge, st_marks[0] = 455; isme abhi [455] value hai ->
-                            // agli baar value dalne pe wah bhi st_marks[0] = [CHANGING VALUES]; isi jagah store hote jayenge ->
-                            // aur agar aakhri me humne iski value daali st_marks[0] = 54875; To values change hote hote ->
-                            // jo aakhri value hogi [54875] wahi print hokar aayega.
-                            // To hum aisi galti nhi karenge, aur user se input Pointer variable me store karenge, aur us pointer me ->
-                            // jo address hoga wahan humare values jaakr store ho jayenge, uske baad jab hum apne pointer ko [marks + 1]
-                            // is tarah se badhayenge to wah next address pe humari Values ko store karta jayega, aur humari values ->
-                            // Array ke us Contiguous memory Location pe jaakar [Pointer Variables ke andar store hue ADDRESS] store ->
-                            // ho jayenge, Jisse hum apne array ki values "st_marks" [Ya Pointer (*) variable ke andar store hue values]
-                            // ko agar print karenge to hum saare Values ko Contiguos memory location pe store bhi kar payenge aur print bhi.
+        scanf("%d", marks);
+        // scanf("%d", st_marks); // ise comment out karke upar waale ko comment karke run kar dekh sakte hain dono
+        // me kya farq hai, isse sirf ik hi elemnent me values store hote rhenge aur [19] me Garbage value hi rhenge.
+        
+        
+        // yahan hum apne inputs user se lekar apne Array me store directly nhi karwayenge, woh kyun dekhte hain ->
+        // scanf("%d", st_marks); Agar hum apne pointer VAriable me store n karakar agar apne [ARRAY] store ->
+        // karwate hain to humari value, us array ke andar store to ho jayengi, LEKIN SIRF AUR SIRF AAKHRI ->
+        // VALUE HI STORE HOGI, HUM JITNI BAAR APNI VALLUE DENGE WAH BAS IK HI ADDRESS PE STORE HONGI AUR ->
+        // HUMESHA WAHIN PE VALUES CHANGE HOTE RAHGENG [ KYUNKI, ARRAY ik tarah ka Pointer hi hai] AUR ->
+        // HUM JITNI BAAR VALUES ENTER KARTE JAYENGE WAH CHANGE HOTE JAYENGE, AUR SABSE AAKHRI VALUE ->
+        // US IK HI ADDRESS PE STORE HOGI. For Example - st_marks[0] = 455;
+        // Hum sirf ik hi array me values ko change karte jayenge, st_marks[0] = 455; isme abhi [455] value hai ->
+        // agli baar value dalne pe wah bhi st_marks[0] = [CHANGING VALUES]; isi jagah store hote jayenge ->
+        // aur agar aakhri me humne iski value daali st_marks[0] = 54875; To values change hote hote ->
+        // jo aakhri value hogi [54875] wahi print hokar aayega.
+        // To hum aisi galti nhi karenge, aur user se input Pointer variable me store karenge, aur us pointer me ->
+        // jo address hoga wahan humare values jaakr store ho jayenge, uske baad jab hum apne pointer ko [marks + 1]
+        // is tarah se badhayenge to wah next address pe humari Values ko store karta jayega, aur humari values ->
+        // Array ke us Contiguous memory Location pe jaakar [Pointer Variables ke andar store hue ADDRESS] store ->
+        // ho jayenge, Jisse hum apne array ki values "st_marks" [Ya Pointer (*) variable ke andar store hue values]
+        // ko agar print karenge to hum saare Values ko Contiguos memory location pe store bhi kar payenge aur print bhi.
 
         marks++;
     }
